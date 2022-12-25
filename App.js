8674a0466e434f45d67ca6febffd1ac72a2448e3
Aadityapburujwale <aadityapburujwale@gmail.com>
2022-12-25 19:06:52 +0530

    API Route Defined

--- App.js
+++ App.js
@@ -8,6 +8,15 @@ app.get('/',(req,res)=>{
     res.send("Hello World..");
 })
 
+app.get('/api',(req,res)=>{
+    res.send(JSON.stringify({name:"Aaditya",age:21}));
+})
+
+app.all(/[a-z]+/, function(req, res) {
+    res.send("Page Not Found!");
+})
+
+
 app.listen(port,()=>{
     console.log(`Server Listening At http://localhost:${port}`);
 })
