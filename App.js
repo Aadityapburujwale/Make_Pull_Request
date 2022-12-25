f4da4468160e1983d2a9decfb45a79cf463c19a2
Aadityapburujwale <aadityapburujwale@gmail.com>
2022-12-25 18:48:01 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,13 @@
+// Import Express Module
+
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/',(req,res)=>{
+    res.send("Hello World..");
+})
+
+app.listen(port,()=>{
+    console.log(`Server Listening At http://localhost:${port}`);
+})
